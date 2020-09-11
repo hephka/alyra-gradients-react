866068a82ccbefc8cf50eaeff2779b9ca7175be7
hephka <franckthevenin95@gmail.com>
2020-09-11 17:52:16 +0200

    add icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from "react"
 
 const Footer = () => {
-    return null
+    return (
+        <footer className="bg-dark text-white p-3 p-lg-5">
+            <p className="text-center mb-0" lang="en">
+            {/* eslint-disable-next-line */}
+                &copy; 2020 Made with 💚 by the Red-Team for Alyra
+        </p>
+        </footer>
+    )
 }
 
 export default Footer
