cd2fc4efb7ca28797e2d240e7a41e522b84a1737
hephka <franckthevenin95@gmail.com>
2020-09-14 09:56:25 +0200

    correction className

--- src/components/Gradient/GradientTag.js
+++ src/components/Gradient/GradientTag.js
@@ -3,7 +3,7 @@ import React from 'react';
 const GradientTag = (props) => {
     const { tags } = props
     return (
-        <div class="d-flex flex-row mb-3">
+        <div className="d-flex flex-row mb-3">
             {tags.map( (el) => (
                 <button 
                     className="d-flex btn btn-sm btn-dark mt-2 mb-1 mr-1" 
